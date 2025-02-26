b9d6107f6df3058c58dd23c8cd3afb1dc26ba272
ReusableChopsticks <gangstapotato36@gmail.com>
2025-02-26 18:32:03 +1100

    added basic html for overall statistics

--- src/ViewPage/ViewPage.tsx
+++ src/ViewPage/ViewPage.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { getData, GoodreadsDataField } from "../Data/repo";
 import { PAGE_HEIGHT_M } from "../Data/constants";
 
+import "./ViewPage.css";
+
 export default function ViewPage() {
   const [books, setBooks] = useState<GoodreadsDataField[]>(getData());
   const totalBooks = getTotalBookCount(books);
@@ -15,65 +17,81 @@ export default function ViewPage() {
     // setbooks(getData());
   }, []);
 
-  return (
-    <>
-      <h1>
-        Overall statistics
-      </h1>
+  return <div className="view-page | even-columns">
+    <div className="statistics-view">
+      <h1>Overall Statistics</h1>
 
-      <h2>Total books</h2>
-      <p>{getTotalBookCount(books)} books</p>
-
-      <h2>Total pages</h2>
-      <p>{getTotalPageCount(books)} pages</p>
-
-      <h2>Average book length</h2>
-      <p>{(totalPages / totalBooks).toFixed()} per book</p>
-
-      <h2>Average star rating</h2>
-      <p>{getAverageStarRating(books).toFixed(2)} stars</p>
-
-      <h2>Average read time</h2>
-      <p>{getAverageReadTime(books)} days</p>
-
-      <h2>Favourite Authors</h2>
-      <p>
-        <ol>
-          {
-            getFavouriteAuthors(books).map((author) => {
-              return <li>{`${author.author}: ${author.readCount}`}</li>
-            })}
-        </ol>
-      </p>
+      <label htmlFor="filter-shelf">
+        Filter by shelf
+        <select id="filter-shelf">
+          <option value="read">read</option>
+          <option value="read">GENERATE BASED ON SHELVES</option>
+        </select>
+      </label>
       
-    </>
-  );
+      <div className="stat-columns | even-columns">
+        <div className="total-pages">
+          <h2>Total pages read</h2>
+          <span>{getTotalPageCount(books)}</span>
+        </div>
+        <div className="total-books">
+          <h2>Total books read</h2>
+          <span>{getTotalBookCount(books)}</span>
+        </div>
+        <div className="avg-pages">
+          <h2>Average book length</h2>
+          <span>{"placeholder"}</span>
+        </div>
+        <div className="avg-stars">
+          <h2>Average star rating</h2>
+          <span>{getAverageStarRating(books).toFixed(2)}</span>
+        </div>
+        <div className="avg-read-time">
+          <h2>Average star rating</h2>
+          <span>{getAverageReadTime(books).toFixed() + " days"}</span>
+        </div>
+
+      </div>
+
+      <button>Back</button>
+    </div>
+    <div className="book-tower">
+      <span>Your book tower is: </span>
+      <span>not tall enough</span>
+    </div>
+  </div>;
 }
 
 const getTotalBookCount = (data: GoodreadsDataField[]) => {
   return data.length;
-}
+};
 
 const getTotalPageCount = (data: GoodreadsDataField[]): number => {
-  return data.reduce((sum, book) => sum += book["Number of Pages"], 0);
-}
+  return data.reduce((sum, book) => (sum += book["Number of Pages"]), 0);
+};
 
 const getAverageStarRating = (data: GoodreadsDataField[]): number => {
   // Exclude books that have no ratings
-  const totalRatings = data.reduce((total, book) => total + (book["My Rating"] > 0 ? 1 : 0), 0);
+  const totalRatings = data.reduce(
+    (total, book) => total + (book["My Rating"] > 0 ? 1 : 0),
+    0
+  );
   if (totalRatings === 0) {
     return 0; // Return 0 if there are no rated books
   }
   const sumRatings = data.reduce((sum, book) => sum + book["My Rating"], 0);
   return sumRatings / totalRatings;
-}
+};
 
 // TODO: if number of books is the same, sort authors by page count!
 // ^^^ this is totally optional because who really cares
-interface AuthorCount { author: string, readCount: number }
+interface AuthorCount {
+  author: string;
+  readCount: number;
+}
 /**
- * 
- * @param data book list 
+ *
+ * @param data book list
  * @returns a sorted list of objects of the top 5 authors by number of books read {author: string, readCount: number}
  */
 const getFavouriteAuthors = (data: GoodreadsDataField[]): AuthorCount[] => {
@@ -85,22 +103,22 @@ const getFavouriteAuthors = (data: GoodreadsDataField[]): AuthorCount[] => {
 
   const sortedAuthors = Object.keys(authorCounts)
     .sort((a, b) => authorCounts[b] - authorCounts[a])
-    .map(author => ({ author, readCount: authorCounts[author] }));
+    .map((author) => ({ author, readCount: authorCounts[author] }));
 
   return sortedAuthors.slice(0, 5);
-}
+};
 
 /**
- * 
+ *
  * @param totalPages total number of pages read
  * @returns the height of your book stack in meters
  */
 const getBookStackHeight = (totalPages: number): number => {
   return totalPages * PAGE_HEIGHT_M;
-}
+};
 
 /**
- * 
+ *
  * @param data book list
  * @returns average time in days to read a book
  */
@@ -111,18 +129,17 @@ const getAverageReadTime = (data: GoodreadsDataField[]): number => {
     if (!book["Date Added"] || !book["Date Read"]) {
       return total;
     }
-    
+
     // find the time difference in days
     const dateAdded = new Date(book["Date Added"]);
     const dateRead = new Date(book["Date Read"]);
 
     const diffTime = Math.abs(dateAdded.getTime() - dateRead.getTime());
-    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); 
-    
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
     validBooks += 1;
     return total + diffDays;
-  }, 0)
-
+  }, 0);
 
   return Math.ceil(totalReadTime / validBooks);
-}
+};
