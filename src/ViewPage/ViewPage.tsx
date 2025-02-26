22726fd0d8b28bfceaff47b93b16b90c71d86dac
ReusableChopsticks <gangstapotato36@gmail.com>
2025-02-27 09:52:28 +1100

    fixed getAverageReadTime

--- src/ViewPage/ViewPage.tsx
+++ src/ViewPage/ViewPage.tsx
@@ -3,11 +3,14 @@ import { getData, GoodreadsDataField } from "../Data/repo";
 import { PAGE_HEIGHT_M } from "../Data/constants";
 
 import "./ViewPage.css";
+import { useNavigate } from "react-router-dom";
 
 export default function ViewPage() {
   const [books, setBooks] = useState<GoodreadsDataField[]>(getData());
   const totalBooks = getTotalBookCount(books);
   const totalPages = getTotalPageCount(books);
+
+  const navigate = useNavigate();
 
   // load the data in on page load
   useEffect(() => {
@@ -47,17 +50,31 @@ export default function ViewPage() {
           <span>{getAverageStarRating(books).toFixed(2)}</span>
         </div>
         <div className="avg-read-time">
-          <h2>Average star rating</h2>
+          <h2>Average read time</h2>
           <span>{getAverageReadTime(books).toFixed() + " days"}</span>
         </div>
 
       </div>
 
-      <button>Back</button>
+      <button onClick={() => navigate("/")}>Back</button>
     </div>
     <div className="book-tower">
-      <span>Your book tower is: </span>
-      <span>not tall enough</span>
+      {/* <span>Your book tower is: </span>
+      <span>not tall enough</span> */}
+      <div className="floor" />
+      <div className="book-spine">hello world nice to see you</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello world</div> 
+      <div className="book-spine">hello</div> 
     </div>
   </div>;
 }
@@ -76,6 +93,7 @@ const getAverageStarRating = (data: GoodreadsDataField[]): number => {
     (total, book) => total + (book["My Rating"] > 0 ? 1 : 0),
     0
   );
+
   if (totalRatings === 0) {
     return 0; // Return 0 if there are no rated books
   }
@@ -118,6 +136,16 @@ const getBookStackHeight = (totalPages: number): number => {
 };
 
 /**
+ * @param dateString date string formatted as DD/MM/YYYY
+ * @returns the time as a date object
+ */
+const processDateString = (dateString: string): Date => {
+  let split = dateString.split("/");
+  // Date requires the string formatted as YYYY-MM-DD to work
+  return new Date(`${split[2]}-${split[1]}-${split[0]}`);
+}
+
+/**
  *
  * @param data book list
  * @returns average time in days to read a book
@@ -125,21 +153,28 @@ const getBookStackHeight = (totalPages: number): number => {
 const getAverageReadTime = (data: GoodreadsDataField[]): number => {
   let validBooks = 0;
   const totalReadTime = data.reduce((total, book) => {
+
+    console.log(book.Title);
     // if both dates not set, do not add to total
     if (!book["Date Added"] || !book["Date Read"]) {
       return total;
     }
 
     // find the time difference in days
-    const dateAdded = new Date(book["Date Added"]);
-    const dateRead = new Date(book["Date Read"]);
+    const dateAdded = processDateString(book["Date Added"]);
+    const dateRead = processDateString(book["Date Read"]);
 
-    const diffTime = Math.abs(dateAdded.getTime() - dateRead.getTime());
+    const diffTime = Math.abs(dateRead.getTime() - dateAdded.getTime());
     const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
 
     validBooks += 1;
     return total + diffDays;
   }, 0);
 
+  console.log(`validBooks: ${validBooks}`);
+  console.log(`totalReadTime: ${totalReadTime}`);
+
+  if (validBooks === 0) { return 0; }
   return Math.ceil(totalReadTime / validBooks);
 };
